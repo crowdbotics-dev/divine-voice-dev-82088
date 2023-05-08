e677a1884fdac9f82162f59566461df02cfe080a
Crowdbotics <engineering@crowdbotics.com>
2023-05-08 11:57:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/divine_voice_dev_82088/MainActivity.java
@@ -0,0 +1,15 @@
+package com.divine_voice_dev_82088;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "divine_voice_dev_82088";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-voice-dev-82088",
+  url: "https://divine-voice-dev-82088.botics.co",
+  api: "https://divine-voice-dev-82088.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
